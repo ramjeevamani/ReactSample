71620a9dc4e81f07a1fac7fe52baaf448d36426d
Ramu Jeevamani <ramujeevamani11@gmail.com>
2023-11-25 11:48:35 +0530

    task2 modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import Component from './Component';
+import SimpleState from './SimpleState'
 
 function App() {
   return (
     <div className="App">
    <h1 >Hello React</h1>
    <button>Click me</button>
-   <Component> </Component>
+   <Component  user= { {name:'Ramu', role:"developer", mail:"rj.jeevamani@gmail.com"}}> </Component>
+   <SimpleState />
    </div>
   );
 }
--- src/Component.jsx
+++ src/Component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 
-function Component(props) {git 
-    console.log(props)
+function Component(props) {
+    const {user} = props
     return <>
   <h1>Hello </h1>
   <h1>Welcome to React Sample Application</h1>
+  <h3>Name : {user.name}</h3>
+  <h3>role : {user.role}</h3>
+  <h3>mail : {user.mail}</h3>
   </>
 }
 
