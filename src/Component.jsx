932342713470dbd67c52db34aeae5e49c345594b
Ramu Jeevamani <ramujeevamani11@gmail.com>
2023-11-25 10:34:11 +0530

    component is modified

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 
-function Component(props) {
+function Component(props) {git 
     console.log(props)
     return <>
   <h1>Hello </h1>
+  <h1>Welcome to React Sample Application</h1>
   </>
 }
 
